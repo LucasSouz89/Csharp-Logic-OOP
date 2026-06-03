ead5d53ae5f2d5924ea06162629f50bfa44c9b84
Lucas Souza <lucas.ss7@outlook.com>
2026-06-03 10:48:09 -0300

    First Model-Controller-View - Department

---
+++ c# POO/Seção 17 - Projeto sistema Web com .NET 2.1 e Entity Frameworks/SalesWebMvc/SalesWebMvc/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public string Name { get; private set; }
+
+        public int ID { get;private set; }
+
+        public Department(string name, int iD)
+        {
+            Name = name;
+            ID = iD;
+        }
+    }
+}

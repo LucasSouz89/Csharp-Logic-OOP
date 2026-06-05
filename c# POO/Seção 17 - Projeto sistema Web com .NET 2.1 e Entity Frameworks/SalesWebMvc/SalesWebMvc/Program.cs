0895b717da9fcfbb90de6f74c81513caed50f1b5
Lucas Souza <lucas.ss7@outlook.com>
2026-06-05 15:55:17 -0300

    SellersController

--- c# POO/Seção 17 - Projeto sistema Web com .NET 2.1 e Entity Frameworks/SalesWebMvc/SalesWebMvc/Program.cs
+++ c# POO/Seção 17 - Projeto sistema Web com .NET 2.1 e Entity Frameworks/SalesWebMvc/SalesWebMvc/Program.cs
@@ -21,6 +21,7 @@ public class Program
         // Add services to the container.
         builder.Services.AddControllersWithViews();
 
+
         var app = builder.Build();
 
         if (app.Environment.IsDevelopment())
